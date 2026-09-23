b3c14b9440fabb89e2a425e9ea191f18dbc59a84
Francesco <francesco1896@gmail.com>
2026-09-24 00:38:19 +0200

    CLI - added tab buttons

---
+++ client/cli/include/ButtonTab.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <ncurses.h>
+#include <unordered_map>
+#include <string>
+#include <functional>
+
+#include <BasicTab.hpp>
+#include <CurseWindow.hpp>
+
+
+class ButtonTab :  public BasicTab
+{
+	public:
+		using BasicTab::BasicTab;
+
+		ButtonTab(std::string const& content, std::function<void()> action, int32_t colorPair = -1, CurseWindow* parent = nullptr);
+
+		ButtonTab(ButtonTab&& other) noexcept;
+		ButtonTab& operator=(ButtonTab&& other) = delete;
+
+		virtual ~ButtonTab(void) { this->clear(); }
+
+		void draw(int32_t h, int32_t w, int32_t y, int32_t x) override;
+		void refresh(void) const noexcept override;
+		void clear(void) noexcept override;
+
+		void activate(void) override;
+		void deactivate(void) override;
+
+		size_t getWidth(void) const noexcept { return this->content.size() + 2; }
+
+	protected:
+		std::string				content;
+		std::function<void()>	action;
+
+		WINDOW*	btnWin{nullptr};
+};
